e289785573a86b7adb478935634cea1500d00df5
Jasper Francisco <jasper.francisco@gmail.com>
2016-02-28 09:25:32 +0800

    added background

--- src/game.js
+++ src/game.js
@@ -3,10 +3,25 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 game.state.add("play", {
   preload: function() {
     game.load.image("skeleton", "assets/allacrost_enemy_sprites/skeleton.png")
+    this.game.load.image('forest-back', 'assets/parallax_forest_pack/layers/parallax-forest-back-trees.png');
+    this.game.load.image('forest-lights', 'assets/parallax_forest_pack/layers/parallax-forest-lights.png');
+    this.game.load.image('forest-middle', 'assets/parallax_forest_pack/layers/parallax-forest-middle-trees.png');
+    this.game.load.image('forest-front', 'assets/parallax_forest_pack/layers/parallax-forest-front-trees.png');
   },
   create: function() {
     var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
     skeletonSprite.anchor.setTo(0.5, 0.5);
+
+    var state = this;
+
+    this.background = this.game.add.group();
+
+    ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+      .forEach(function(image){
+        var bg = state.game.add.tileSprite(0, 0, state.game.world.width,
+          state.game.world.height, image, '', state.background);
+        bg.tileScale.setTo(4,4);
+      });
   },
   render: function() {
     game.debug.text("Adventure Awaits!", 250, 290);
